aed1df9ce9179cb68fee1defdbffc062d2badee8
Only-San <duysannguyen113@gmail.com>
2026-03-27 21:32:54 +0700

    Update route.ts
    fix

--- be-user/app/api/user/route.ts
+++ be-user/app/api/user/route.ts
@@ -3,6 +3,5 @@ import { NextRequest,NextResponse } from "next/server";
 
 export async function GET(request:Request) {
     const db=await connectDB()
-    const [rows]=await db.execute('SELECT * from users')
     return NextResponse.json(rows)
 }
