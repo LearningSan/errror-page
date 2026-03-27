90b478fbcc377f1ef332448097b69b15769a7c38
Only-San <duysannguyen113@gmail.com>
2026-03-27 21:37:04 +0700

    Delete be-user/app/api/user/route.ts

--- be-user/app/api/user/route.ts
+++
@@ -1,7 +0,0 @@
-import { connectDB } from "@/app/lib/data";
-import { NextRequest,NextResponse } from "next/server";
-
-export async function GET(request:Request) {
-    const db=await connectDB()
-    return NextResponse.json(rows)
-}
